6f4edb440d0dc14958a95b6fe16052a9319779ba
Maciej Mycielski <maciejmycielski@gmail.com>
2020-07-31 16:36:27 +0200

    Base views added

---
+++ books/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import FillDatabaseAPIView, ListBookAPIView, RetrieveBookAPIView
+app_name = 'books'
+
+urlpatterns = [
+    path('db', FillDatabaseAPIView.as_view(), name="fill_db"),
+    path('books', ListBookAPIView.as_view(), name="books_list"),
+    path('books/<id>', RetrieveBookAPIView.as_view(), name="retrieve_book"),
+    ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
 
+from .serializers import CreateBookSerializer, BookSerializer
+from .models import Book
 # Create your views here.
+from rest_framework.views import APIView
+
+
+class FillDatabaseAPIView(APIView):
+    serializer_class = CreateBookSerializer
+
+    def post(self, request):
+        if "q" in request.data and request.data["q"] == "war":
+            return Response("Database filled.", status=status.HTTP_200_OK)
+        return Response("Wrong parameters", status=status.HTTP_401_UNAUTHORIZED)
+
+
+class ListBookAPIView(APIView):
+    serializer_class = BookSerializer
+
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = self.serializer_class(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class RetrieveBookAPIView(APIView):
+    serializer_class = BookSerializer
+
+    def get(self, request, *args, **kwargs):
+        try:
+            book = Book.objects.get(id=self.kwargs["id"])
+            serializer = self.serializer_class(book)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Book.DoesNotExist:
+            return Response("Book with this ID not found!", status=status.HTTP_401_UNAUTHORIZED)
