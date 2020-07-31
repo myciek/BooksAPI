a978d827e7fb90617958020a954f2766a8335103
Maciej Mycielski <maciejmycielski@gmail.com>
2020-07-31 15:38:26 +0200

    Book model added

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
 
 # Create your models here.
+class Book(models.Model):
+    title = models.TextField(max_length=255)
+    authors = ArrayField(models.TextField(max_length=255))
+    published_date = models.TextField(max_length=10)
+    categories = ArrayField(models.TextField(max_length=255))
+    average_rating = models.DecimalField(max_digits=2, decimal_places=1, blank=True)
+    ratings_count = models.IntegerField(blank=True)
+    thumbnail = models.TextField(max_length=1024)
