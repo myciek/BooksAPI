7f6494b472b934e8d39c7b71093c9a640ce555b6
Maciej Mycielski <maciejmycielski@gmail.com>
2020-07-31 16:05:19 +0200

    Serializer added

---
+++ books/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Book
+
+
+class CreateBookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = (
+            "id", "title", "authors", "published_date", "categories", "average_rating", "raiting_count", "thumbnail"
+        )
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = (
+             "title", "authors", "published_date", "categories", "average_rating", "raiting_count", "thumbnail"
+        )
